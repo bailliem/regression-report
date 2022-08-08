b9ef4466b5b7e445057cab8cdca09cba07efeb0a
Georg Heinze <georg.heinze@meduniwien.ac.at>
2022-08-08 17:04:30 +0200

    included pseudo_log_explainer, changed criterion for accepting pseudo-log to delta-corr=0.2, references in SAP

--- R/ida_trans.R
+++ R/ida_trans.R
@@ -16,20 +16,25 @@ ida_trans<-function(x, equ.marg=0.05){
   qn<-qnorm(xr,0,1)
   
   cn.orig<-cornorm(x, qn)
-  
-  
-  
-  if(min(x, na.rm=T)>0) interval=c(0,max(x))
-  else interval=c(min(x[x>0]), max(x))
-  
-  topt<-optimize(f=function(const) 1-cornorm(X=pseudo_log(x, sigma=const), qn=qn), interval=interval, tol=0.05)
-  
-  if(topt$objective>(1-cn.orig-equ.marg)) {
-    #cat("Original distribution better than any log.\n")
-    res<-list(const=NA, fun=function(x) x)
+  if(cn.orig > 1-equ.marg){
+    res <- list(const=NA, fun=function(x) x)
   } else {
-    res<-list(const=topt$minimum, fun=function(x) log(x))
-  } 
+  
+#    if(min(x, na.rm=T)>0) interval=c(0,max(x))
+#    else interval=c(min(x[x>0]), max(x))
+    interval <- c(-10,10)
+    
+#    topt<-optimize(f=function(const) 1-cornorm(X=pseudo_log(x, sigma=const), qn=qn), interval=interval, tol=0.05)
+    topt<-optimize(f=function(const) 1-cornorm(X=pseudo_log(x, sigma=2**const), qn=qn), interval=interval, tol=0.5)
+    
+    if(topt$objective>(1-cn.orig-equ.marg)) {
+      #cat("Original distribution better than any log.\n")
+      res<-list(const=NA, fun=function(x) x)
+    } else {
+      #res<-list(const=topt$minimum, fun=function(x) log(x))
+      res<-list(const=2**topt$minimum, fun=function(x) log(x))
+    } 
+  }
   return(res)
 }
 
